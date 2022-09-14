e4df64513349c9d719bf99bafdf6edf612be2ead
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-09-14 23:00:20 +0300

    create useToggle hook

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,10 +1,15 @@
 // import ThemeSwitch from '../ThemeSwitch'
-import { Header } from './AppBar.styled'
+import Button from '../Button'
+import { Header, Logo, My, Phonebook } from './AppBar.styled'
 
 export default function AppBar() {
   return (
     <Header>
-      {/* <ThemeSwitch onSwitchTheme={onSwitchTheme} themeBool={themeBool} /> */}
+      <Logo>
+        <My>My</My>
+        <Phonebook>Phonebook</Phonebook>
+      </Logo>
+      <Button>+</Button>
     </Header>
   )
 }
--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -5,12 +5,25 @@ export const Header = styled.header`
   top: 0;
   left: 0;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 12px 20px;
+  /* justify-content: center;
+  align-items: center; */
+  padding: 15px 20px;
   height: 72px;
   z-index: 1100;
   color: ${(props) => props.theme.mainTextColor};
   background-color: ${(props) => props.theme.accentColor};
   box-shadow: ${(props) => props.theme.appBarBoxShadow};
 `
+
+export const Logo = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const My = styled.p`
+  font-size: 20px;
+  line-height: 1;
+  font-weight: 900;
+`
+
+export const Phonebook = styled(My)``
