8223d2b0ad84d6764f23799f44432d70c849cf59
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-10-25 22:32:05 +0300

    SignupView logic mvp

--- src/views/SignupView.jsx
+++ src/views/SignupView.jsx
@@ -1,5 +1,56 @@
-import React from 'react'
+import React, { useId, useState } from 'react'
 
 export default function SignupView() {
-  return <div>SignupView</div>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    if (name === 'name') setName(value)
+    if (name === 'email') setEmail(value)
+    if (name === 'password') setPassword(value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const user = { name, email, password }
+    console.log(user)
+  }
+
+  const id = useId()
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor={id + '-name'}>Name</label>
+        <input
+          onChange={handleChange}
+          id={id + '-name'}
+          value={name}
+          name="name"
+          type="text"
+          required
+        />
+        <label htmlFor={id + '-email'}>Email</label>
+        <input
+          onChange={handleChange}
+          id={id + '-email'}
+          value={email}
+          name="email"
+          type="email"
+          required
+        />
+        <label htmlFor={id + '-password'}>Password</label>
+        <input
+          onChange={handleChange}
+          id={id + '-password'}
+          value={password}
+          name="password"
+          type="password"
+          required
+        />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  )
 }
