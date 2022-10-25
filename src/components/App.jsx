051456d1d3ef07a911f87e5c04f1ee7b12eee4a4
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-10-25 21:57:48 +0300

    base routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,20 @@ import GlobalStyle from '../styles'
 import { light } from '../theme'
 import { Route, Routes } from 'react-router-dom'
 import SharedLayout from './SharedLayout'
+import LoginView from '../views/LoginView'
+import SignupView from '../views/SignupView'
 
 export default function App() {
   return (
     <>
       <ThemeProvider theme={light}>
         <GlobalStyle />
-
         <Routes>
           <Route path="/" element={<SharedLayout />}>
             <Route index element={<ContactsView />}></Route>
             <Route path="contacts" element={<ContactsView />}></Route>
-            <Route path="login"></Route>
-            <Route path="signup"></Route>
+            <Route path="login" element={<LoginView />}></Route>
+            <Route path="signup" element={<SignupView />}></Route>
           </Route>
         </Routes>
       </ThemeProvider>
